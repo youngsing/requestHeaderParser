511d3f58092479041bb42b0bbfe39300433cdfcc
youngsing <youngsingyx@gmail.com>
2016-12-03 01:21:43 +0800

    init-commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.get('*', function (req, res) {
+	var result = {};
+	
+	var sys = req.headers['user-agent'];
+	sys = sys.match(/\((.+?)\)/)[1];
+	result['software'] = sys;
+	
+	var lng = req.headers['accept-language'];
+	lng = lng.split(';')[0].split(",")[0];
+	result['language'] = lng;
+	
+	var ip = req.headers[''];
+	result['idaddress'] = ip;
+	
+	res.json(result);
+});
+
+app.listen(8080, function () {
+	console.log("Listening on port 8080!");
+});
